0a10595ad12dbfa7eb9a90ce9816464c1ed8e950
TroyWM <troy.w.miller.1994@gmail.com>
2023-04-24 01:02:09 -0500

    Cards are opened are displayed

--- card_client_test/src/App.js
+++ card_client_test/src/App.js
@@ -4,6 +4,27 @@ import './App.css';
 var x = "no test";
 var pack = [];
 
+//Table for static url lookup.
+const cards = {
+  "common-1":"https://cdn.discordapp.com/attachments/1075194391616032808/1099856680466010162/Common-1.png",
+  "common-2":"https://cdn.discordapp.com/attachments/1075194391616032808/1099856680738631791/Common-2.png",
+  "common-3":"https://cdn.discordapp.com/attachments/1075194391616032808/1099856678540820511/Common-3.png",
+  "common-4":"https://cdn.discordapp.com/attachments/1075194391616032808/1099856678788288672/Common-4.png",
+  "common-5":"https://cdn.discordapp.com/attachments/1075194391616032808/1099856679039938570/Common-5.png",
+  "uncommon-1":"https://cdn.discordapp.com/attachments/1075194391616032808/1099856719640809482/Uncommon-1.png",
+  "uncommon-2":"https://cdn.discordapp.com/attachments/1075194391616032808/1099856719913423019/Uncommon-2.png",
+  "uncommon-3":"https://cdn.discordapp.com/attachments/1075194391616032808/1099856720169287690/Uncommon-3.png",
+  "uncommon-4":"https://cdn.discordapp.com/attachments/1075194391616032808/1099856717908557895/Uncommon-4.png",
+  "uncommon-5":"https://cdn.discordapp.com/attachments/1075194391616032808/1099856718122471424/Uncommon-5.png",
+  "rare-1":"https://cdn.discordapp.com/attachments/1075194391616032808/1099856679329349672/Rare-1.png",
+  "rare-2":"https://cdn.discordapp.com/attachments/1075194391616032808/1099856679664898179/Rare-2.png",
+  "rare-3":"https://cdn.discordapp.com/attachments/1075194391616032808/1099856679950102539/Rare-3.png",
+  "superRare-1":"https://cdn.discordapp.com/attachments/1075194391616032808/1099856718659342416/Super_Rare-1.png",
+  "superRare-2":"https://cdn.discordapp.com/attachments/1075194391616032808/1099856718910984312/Super_Rare-2.png",
+  "superRare-3":"https://cdn.discordapp.com/attachments/1075194391616032808/1099856719389147166/SuperRare_3.png",
+}
+
+
 async function sayHello() {
   const response = await fetch("http://localhost:5000/user/open", {
     method: "GET",
@@ -53,11 +74,11 @@ function displayCard(){
   }
 
 
-  document.getElementById("cardOne").innerHTML = packs[0];
-  document.getElementById("cardTwo").innerHTML = packs[1];
-  document.getElementById("cardThree").innerHTML = packs[2];
-  document.getElementById("cardFour").innerHTML = packs[3];
-  document.getElementById("cardFive").innerHTML = packs[4];
+  document.getElementById("cardOne").innerHTML = "<img src="+String(cards[packs[0]] )+">";
+  document.getElementById("cardTwo").innerHTML = "<img src="+String(cards[packs[1]] )+">";
+  document.getElementById("cardThree").innerHTML = "<img src="+String(cards[packs[2]] )+">";
+  document.getElementById("cardFour").innerHTML = "<img src="+String(cards[packs[3]] )+">";
+  document.getElementById("cardFive").innerHTML = "<img src="+String(cards[packs[4]] )+">";
 
 }
 
@@ -65,7 +86,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -80,11 +100,14 @@ function App() {
             <button onClick={sayHello}>Default</button>
             <div className="col-md">
             <button onClick={displayCard}>Second button</button>
+            <h2>            
             <h1 id="cardOne">no test</h1>
             <h1 id="cardTwo">no test</h1>
             <h1 id="cardThree">no test</h1>
             <h1 id="cardFour">no test</h1>
             <h1 id="cardFive">no test</h1>
+            </h2>
+
         </div>
 
 
